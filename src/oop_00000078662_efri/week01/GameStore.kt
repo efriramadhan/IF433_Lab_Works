785c9e4f17980f6ff19c9310443f10d91a3f1cbf
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-02-09 01:10:54 +0700

    week01: (Tugas 2) implement receipt printing

--- src/oop_00000078662_efri/week01/GameStore.kt
+++ src/oop_00000078662_efri/week01/GameStore.kt
@@ -1,9 +1,18 @@
 package oop_00000078662_efri.week01
 
 fun main() {
-    val gametitle = "Elden Ring"
-    val price = 600000
+    val gameTitle = "Elden Ring"
+    val price = 600_000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice
+    )
 }
+
 
 fun calculateDiscount (price: Int) {
     if (price > 5000000) {
