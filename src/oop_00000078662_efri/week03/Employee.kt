d42438ceaef965f2bdc08fbe0b376abc5fd2e8a8
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-02-18 10:11:20 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000078662_efri/week03/Employee.kt
+++ src/oop_00000078662_efri/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000078662_efri.week03
 
 class Employee(val name: String){
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            field = value
+        }
 }
