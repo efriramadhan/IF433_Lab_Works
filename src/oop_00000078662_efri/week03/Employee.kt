e124a85b9307729fb836b9ad249e2e16becf56c4
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-02-18 10:22:32 +0700

    week03: add private property

--- src/oop_00000078662_efri/week03/Employee.kt
+++ src/oop_00000078662_efri/week03/Employee.kt
@@ -1,13 +1,22 @@
 package oop_00000078662_efri.week03
 
 class Employee(val name: String){
-    var salary: Int = 0
-        set(value) {
-            if (value < 0 ){
-                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0
-            } else {
-                field = value
+    private var performanceRating: Int = 3
+        var salary: Int = 0
+            set(value) {
+                if (value < 0 ){
+                    println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                    field = 0
+                } else {
+                    field = value
+                }
             }
-        }
+    fun increasePerformace(){
+        performanceRating++
+        println("Kinerja $name meningkat! Rating $performanceRating")
+    }
+
+    fun printStatus(){
+        println("Karyawan: $name, Rating $performanceRating")
+    }
 }
