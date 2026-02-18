51e1b6d5ec097f17f3d39b9a7e4f45cb844a3259
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-02-18 10:14:05 +0700

    week03: fix setter using backing field

--- src/oop_00000078662_efri/week03/Employee.kt
+++ src/oop_00000078662_efri/week03/Employee.kt
@@ -3,7 +3,11 @@ package oop_00000078662_efri.week03
 class Employee(val name: String){
     var salary: Int = 0
         set(value) {
-            println("Mencoba set gaji ke: $value")
-            field = value
+            if (value < 0 ){
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
         }
 }
