0cfc93169109b960127892eda05839a38008e86e
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-02-18 10:29:09 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000078662_efri/week03/Main.kt
+++ src/oop_00000078662_efri/week03/Main.kt
@@ -2,6 +2,11 @@ package oop_00000078662_efri.week03
 
 fun main(){
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    e.salary = -1000
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformace()
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
