e61c2056a7ce3c4be5cb43e822f82d4307db37a8
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-02-18 10:54:20 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_00000078662_efri/week03/tugas2/Player.kt
@@ -0,0 +1,25 @@
+package oop_00000078662_efri.week03.tugas2
+
+class Player(
+    val username: String
+) {
+
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount <= 0) {
+            println("XP harus bernilai positif!")
+            return
+        }
+
+        val oldLevel = level
+        xp += amount
+
+        if (level > oldLevel) {
+            println("Level Up! Selamat $username naik ke level $level")
+        }
+    }
+}
