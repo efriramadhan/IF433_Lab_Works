2af79506a6fa6af347e20dafa21a9efe2c2f349a
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-02-18 10:31:27 +0700

    week03: cleanup code

--- src/oop_00000078662_efri/week03/Main.kt
+++ src/oop_00000078662_efri/week03/Main.kt
@@ -3,14 +3,11 @@ package oop_00000078662_efri.week03
 fun main(){
     val e = Employee("Budi")
 
-    // 1. Test validasi salary
     e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    // 2. Test encapsulation
     e.increasePerformace()
 
-    // 3. Test computed propertys
     println("Pajak yang harus dibayar: ${e.tax}")
 }
