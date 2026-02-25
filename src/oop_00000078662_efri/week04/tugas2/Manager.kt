b43c3ff93b9a82f5f60bc9c461a3fc817b737c42
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-02-25 10:28:29 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_00000078662_efri/week04/tugas2/Manager.kt
@@ -0,0 +1,15 @@
+package oop_00000078662_efri.week04.tugas2
+
+class Manager(
+    name: String,
+    baseSalary: Int
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500_000
+    }
+}
