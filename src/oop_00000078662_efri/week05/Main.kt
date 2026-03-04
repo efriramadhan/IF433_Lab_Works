54d34129863adf6f738b28d21afe61920e45df78
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-04 10:18:56 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000078662_efri/week05/Main.kt
@@ -0,0 +1,24 @@
+package oop_00000078662_efri.week05
+
+fun main() {
+    val dosen1 = Dosen (nama = "Pak Alex", nidn = "0123455");
+    val admin1 = Admin (nama = "Bu Siti");
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1);
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when(pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("------------------------")
+    }
+}
