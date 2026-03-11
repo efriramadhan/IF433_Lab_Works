7352616b3600def447bca1970fe03eb7c96e3a5f
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-11 10:25:08 +0700

    week06: implement decoupled processCheckout in main

---
+++ src/oop_00000078662_efri/week06/Main.kt
@@ -0,0 +1,23 @@
+package oop_00000078662_efri.week06
+
+import jdk.jfr.DataAmount
+
+fun processCheckout(method: PaymentMethod, amount: Double){
+    println("-> Memulai checkout...")
+    method.pay(amount)
+}
+
+fun main() {
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = Smarthphone()
+    myPhone.turnOn()
+
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== TESTING CHECKOUT ===")
+    processCheckout(method = pay1, amount = 50000.0)
+    processCheckout(method = pay2, amount = 150000.0)
+}
