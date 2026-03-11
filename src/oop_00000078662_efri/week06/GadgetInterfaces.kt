6b7721306147d76358497df2181b2e1d8b28a9e3
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-11 10:10:44 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000078662_efri/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000078662_efri.week06
+
+interface Camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
