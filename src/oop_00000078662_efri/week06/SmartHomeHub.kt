beb730c4c9d1555dd62b4c4756aba80e42ae5159
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-11 10:49:27 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000078662_efri/week06/SmartHomeHub.kt
@@ -0,0 +1,18 @@
+package oop_00000078662_efri.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("${device.name} berhasil ditambahkan ke Smart Home Hub.")
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
