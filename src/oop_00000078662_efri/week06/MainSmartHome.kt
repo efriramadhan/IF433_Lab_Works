6b977ecfa7c0b962bb8f656942f096e19718f77c
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-11 10:52:25 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000078662_efri/week06/MainSmartHome.kt
@@ -0,0 +1,13 @@
+package oop_00000078662_efri.week06
+
+fun MainSmartHome(){
+    val lamp = SmartLamp("1", "Lampu Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+}
