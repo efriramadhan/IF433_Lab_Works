dcbdb1bce2a85d968fa07491f0794f301b2da655
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-11 10:15:56 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000078662_efri/week06/Smarthphone.kt
+++ src/oop_00000078662_efri/week06/Smarthphone.kt
@@ -1,5 +1,9 @@
 package oop_00000078662_efri.week06
 
 class Smarthphone : Camera, Phone {
-    p
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smarthphone berhasil booting.")
+    }
 }
