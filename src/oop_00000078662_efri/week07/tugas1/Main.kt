a72ced9c0aa2260fd9164f4e18dce7c8c7c0acbd
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-30 15:53:59 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000078662_efri/week07/tugas1/Main.kt
+++ src/oop_00000078662_efri/week07/tugas1/Main.kt
@@ -1,7 +1,13 @@
 package oop_00000078662_efri.week07.tugas1
 
 fun main(){
-    println("\n=== CHECKPOINT 18: GAME MANAGER ===")
+    println("\n=== GAME MANAGER ===")
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n=== RARITY & FACTORY ===")
+    println("Drop chance LEGENDARY: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    val starterWeapon = Weapon.forgeStarterSword()
+    println("Senjata: ${starterWeapon.item.name}, Damage: ${starterWeapon.item.damage}, Durability: ${starterWeapon.durability}")
 }
