11a1d1ccd80f0b7ae837d55b981e0a6b325a1b96
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-30 14:30:04 +0700

    week07: implement companion object as factory in NetworkClient

--- src/oop_00000078662_efri/week07/NetworkClient.kt
+++ src/oop_00000078662_efri/week07/NetworkClient.kt
@@ -1,6 +1,16 @@
 package oop_00000078662_efri.week07
 
 class NetworkClient private constructor(val url: String) {
+
+    companion object {
+        const val BASE_URL = "https://www.googleapis.com/"
+
+        fun createClient(): NetworkClient {
+            println("Membangun NetworkClient dengan BASE_URL: $BASE_URL")
+            return NetworkClient("$BASE_URL")
+        }
+    }
+
     fun connect(){
         println("Connecting to $url...")
     }
