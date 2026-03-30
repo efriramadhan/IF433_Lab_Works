566c8a81a98cd0918c6a2c543c18aeee00f58624
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-03-30 15:47:49 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_00000078662_efri/week07/tugas1/BattleEvent.kt
@@ -0,0 +1,12 @@
+package oop_00000078662_efri.week07.tugas1
+
+sealed class BattleState {
+
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+
+    data class LootDropped(val item: GameItem) : BattleState()
+
+    data class GameOver(val reason: String) : BattleState()
+
+    object SafeZone : BattleState()
+}
