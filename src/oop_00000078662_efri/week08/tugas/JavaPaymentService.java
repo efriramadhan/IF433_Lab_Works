027543b16dce57390f65e0c0b6d2152838c02acc
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-04-17 00:06:01 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000078662_efri/week08/tugas/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000078662_efri.week08.tugas;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
