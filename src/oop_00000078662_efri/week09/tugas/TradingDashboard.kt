18a5e33b935ce5ae2e7b869ca31832a766aa31c3
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-04-23 01:01:00 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_00000078662_efri/week09/tugas/TradingDashboard.kt
+++ src/oop_00000078662_efri/week09/tugas/TradingDashboard.kt
@@ -36,4 +36,10 @@ fun main() {
 
     println("\n--- TOP PERFORMERS ---")
     topPerformersString.forEach { println(it) }
+
+    println("\n--- WORST PERFORMERS ---")
+    worstPerformersString.forEach { println(it) }
+
+    println("\n--- TRADED PAIRS ---")
+    println(uniquePairs)
 }
