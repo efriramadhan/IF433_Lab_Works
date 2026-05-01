9ade3e7a47de85568ed402fee3de1a45448f9560
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-05-01 19:27:46 +0700

    week10: (task) populate coin repository

--- src/oop_00000078662_efri/week10/tugas/CryptoDashboard.kt
+++ src/oop_00000078662_efri/week10/tugas/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000078662_efri.week10.tugas
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
