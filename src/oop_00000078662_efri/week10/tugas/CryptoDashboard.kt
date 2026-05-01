6c03cedf36bed70866f27c99fa5a5e06ce91b7cd
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-05-01 19:30:52 +0700

    week10: (task) test full generic crypto architecture

--- src/oop_00000078662_efri/week10/tugas/CryptoDashboard.kt
+++ src/oop_00000078662_efri/week10/tugas/CryptoDashboard.kt
@@ -20,4 +20,9 @@ fun main() {
     txRepo.add(Transaction("TX001", 500.0))
     txRepo.add(Transaction("TX002", 1200.0))
     txRepo.add(Transaction("TX003", 300.0))
+
+    println("\n=== DATA TRANSACTION ===")
+    txRepo.getAll().forEach {
+        println("ID: ${it.id}, Amount: ${it.amount}")
+    }
 }
