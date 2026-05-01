baac0d33e89e4a74e4b93a2907b79d4619afbc57
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-05-01 19:24:05 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000078662_efri/week10/tugas/WalletRepository.kt
@@ -0,0 +1,14 @@
+package oop_00000078662_efri.week10.tugas
+
+class WalletRepository<T> {
+
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
