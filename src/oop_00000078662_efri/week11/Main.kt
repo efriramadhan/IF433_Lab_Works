e7c7b104ad8040f84618a558d6e7d4a6ea01f27d
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-05-06 09:40:12 +0700

    week11: test extension functions in main

---
+++ src/oop_00000078662_efri/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000078662_efri.week11
+
+fun main(){
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
