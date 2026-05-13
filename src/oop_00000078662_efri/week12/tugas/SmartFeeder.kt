73ec6f75dc161e5c9ca7631ff065d9603baee73f
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-05-13 10:46:51 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000078662_efri/week12/tugas/SmartFeeder.kt
@@ -0,0 +1,14 @@
+package oop_00000078662_efri.week12.tugas
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+
+    return availableGram
+}
