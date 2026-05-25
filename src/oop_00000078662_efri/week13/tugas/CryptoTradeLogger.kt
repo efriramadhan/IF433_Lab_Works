b7d5e0eebe4b4ffc3378e77b87fd3910c237d1fa
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-05-25 21:01:03 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000078662_efri/week13/tugas/CryptoTradeLogger.kt
@@ -0,0 +1,11 @@
+package oop_00000078662_efri.week13.tugas
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
