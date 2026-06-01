13aef4c101ba80e3ddf735b5ffeca528cdc18d5d
Efri <126165266+efriramadhan@users.noreply.github.com>
2026-06-01 13:20:00 +0700

    week14: segregate device interfaces

---
+++ src/oop_00000078662_efri/week14/IspRefactored.kt
@@ -0,0 +1,11 @@
+package oop_00000078662_efri.week14
+
+package oop_<nim>_<nama>.week14
+
+interface Printable {
+    fun print(doc: String)
+}
+
+interface Scannable {
+    fun scan(doc: String): String
+}
